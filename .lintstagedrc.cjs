7bdf2757551edf4fa36bf6c8a3c25cb09cdd0ff5
TaumuLu <972409545@qq.com>
2024-11-29 00:49:34 +0800

    fix: lintstagedrc config

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix'],
+  '*.{js,jsx,ts,tsx,vue}': ['eslint --fix'],
   '*.{json,md}': ['prettier --write'],
   '*.{css,less,scss}': ['stylelint --fix'],
   '*.{png,jpg,jpeg}': ['tinypng', 'git add ./tinypng-output'],
